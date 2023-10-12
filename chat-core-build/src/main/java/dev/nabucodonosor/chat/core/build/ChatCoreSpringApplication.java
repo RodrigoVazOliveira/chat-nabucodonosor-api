a2681f933d00279a5fb1066eb04fdd72c4e128c1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-10-12 02:09:16 -0300

    Recreate modules

---
+++ chat-core-build/src/main/java/dev/nabucodonosor/chat/core/build/ChatCoreSpringApplication.java
@@ -0,0 +1,11 @@
+package dev.nabucodonosor.chat.core.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCoreSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatCoreSpringApplication.class, args);
+	}
+}
